a2a447e01422bf919965ca9f4107cb0b4ca3c275
William McKenna <spacevacation@gmail.com>
2014-03-07 01:21:00 -0500

    how broken, hoboken?
    no joke, NJ

---
+++ js/eventListeners.js
@@ -0,0 +1,21 @@
+Reveal.addEventListener( 'slidechanged', function( event ) {
+	// event.previousSlide, event.currentSlide, event.indexh, event.indexv
+	MathJax.Hub.Rerender();  // ctrlpt #010
+
+} );
+
+Reveal.addEventListener( 'customevent', function() {
+	console.log( '"customevent" has fired' );
+} );
+
+Reveal.addEventListener( 'cloud', function() {
+	console.log( '"cloud" has fired' );
+} );
+
+
+Reveal.addEventListener( 'fragmentshown', function( event ) {
+	setTimeout(
+	   		function() {
+				MathJax.Hub.Rerender(document.querySelector(".slides .present")) 
+	   		}, 100);   // ctrlpt #010
+} );
